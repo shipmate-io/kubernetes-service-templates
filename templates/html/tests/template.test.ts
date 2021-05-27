b2a620cda792d0fd9e2d66709919f6949b832c5b
Michiel Kempen <kempenmichiel@gmail.com>
2021-05-27 23:42:11 +0200

    update tests

--- templates/html/tests/template.test.ts
+++ templates/html/tests/template.test.ts
@@ -1,5 +1,6 @@
 import { Template } from 'tests'
 import path from 'path'
+import Service from '@/Service'
 
 const htmlTemplate = new Template(path.resolve(__dirname, '../'))
 
@@ -9,19 +10,16 @@ test('the syntax of the template is valid', async () => {
 
 })
 
-test('the template cannot be parsed without path_to_source_code', async () => {
+test('the template can be parsed without path_to_source_code', async () => {
 
-    const expectedErrors = {
-        path_to_source_code: [ 'The path to html source code field is required.' ],
-    }
+    const parsing = await htmlTemplate.parse('app', 'website')
 
-    const parsing = htmlTemplate.parse('app', 'website')
-
-    await expect(parsing).toFailDueToIncorrectFormInput(expectedErrors)
+    await expect(parsing).toSucceed()
+    await expect(parsing).toMatchParsedTemplate(__dirname+'/concerns/parsed_template_without_path_to_source_code.yml')
 
 })
 
-test('the template can be parsed', async () => {
+test('the template can be parsed with path_to_source_code', async () => {
 
     const variables = {
         'path_to_source_code': 'src/',
@@ -30,11 +28,25 @@ test('the template can be parsed', async () => {
     const parsing = await htmlTemplate.parse('app', 'website', variables)
 
     await expect(parsing).toSucceed()
-    await expect(parsing).toMatchParsedTemplate(__dirname+'/concerns/parsed_template.yml')
+    await expect(parsing).toMatchParsedTemplate(__dirname+'/concerns/parsed_template_with_path_to_source_code.yml')
 
 })
 
-test("the service works correctly when installed", async () => {
+test("the service works correctly when installed without path_to_source_code", async () => {
+
+    const codeRepositoryPath = path.resolve(__dirname, 'concerns/application/')
+
+    const service = await htmlTemplate.install(codeRepositoryPath)
+
+    try {
+        await assertThatServiceWorksAsExpected(service)
+    } finally {
+        await service.uninstall()
+    }
+
+}, 1000 * 60 * 3)
+
+test("the service works correctly when installed with path_to_source_code", async () => {
 
     const codeRepositoryPath = path.resolve(__dirname, 'concerns/application/')
 
@@ -45,39 +57,42 @@ test("the service works correctly when installed", async () => {
     const service = await htmlTemplate.install(codeRepositoryPath, variables)
 
     try {
-
-        const host = `http://localhost:${service.getEntrypoint('html')?.host_port}`
-
-        expect((await page.goto(`${host}/`))?.status()).toBe(200)
-        expect(page.url()).toEqual(`${host}/`)
-        expect(await page.content()).toContain('You are viewing the home page.')
-
-        expect((await page.goto(`${host}/index.html`))?.status()).toBe(200)
-        expect(page.url()).toEqual(`${host}/`)
-        expect(await page.content()).toContain('You are viewing the home page.')
-
-        expect((await page.goto(`${host}/hello`))?.status()).toBe(200)
-        expect(page.url()).toEqual(`${host}/hello`)
-        expect(await page.content()).toContain('You are viewing the hello page.')
-
-        expect((await page.goto(`${host}/hello.html`))?.status()).toBe(200)
-        expect(page.url()).toEqual(`${host}/hello`)
-        expect(await page.content()).toContain('You are viewing the hello page.')
-
-        expect((await page.goto(`${host}/hello/team`))?.status()).toBe(200)
-        expect(page.url()).toEqual(`${host}/hello/team`)
-        expect(await page.content()).toContain('You are viewing the team page.')
-
-        expect((await page.goto(`${host}/hello/team.html`))?.status()).toBe(200)
-        expect(page.url()).toEqual(`${host}/hello/team`)
-        expect(await page.content()).toContain('You are viewing the team page.')
-
-        expect((await page.goto(`${host}/baz`))?.status()).toBe(404)
-        expect(page.url()).toEqual(`${host}/baz`)
-        expect(await page.content()).toContain('Woops, page not found!')
-
+        await assertThatServiceWorksAsExpected(service)
     } finally {
         await service.uninstall()
     }
 
 }, 1000 * 60 * 3)
+
+async function assertThatServiceWorksAsExpected(service: Service) 
+{
+    const host = `http://localhost:${service.getEntrypoint('html')?.host_port}`
+
+    expect((await page.goto(`${host}/`))?.status()).toBe(200)
+    expect(page.url()).toEqual(`${host}/`)
+    expect(await page.content()).toContain('You are viewing the home page.')
+
+    expect((await page.goto(`${host}/index.html`))?.status()).toBe(200)
+    expect(page.url()).toEqual(`${host}/`)
+    expect(await page.content()).toContain('You are viewing the home page.')
+
+    expect((await page.goto(`${host}/hello`))?.status()).toBe(200)
+    expect(page.url()).toEqual(`${host}/hello`)
+    expect(await page.content()).toContain('You are viewing the hello page.')
+
+    expect((await page.goto(`${host}/hello.html`))?.status()).toBe(200)
+    expect(page.url()).toEqual(`${host}/hello`)
+    expect(await page.content()).toContain('You are viewing the hello page.')
+
+    expect((await page.goto(`${host}/hello/team`))?.status()).toBe(200)
+    expect(page.url()).toEqual(`${host}/hello/team`)
+    expect(await page.content()).toContain('You are viewing the team page.')
+
+    expect((await page.goto(`${host}/hello/team.html`))?.status()).toBe(200)
+    expect(page.url()).toEqual(`${host}/hello/team`)
+    expect(await page.content()).toContain('You are viewing the team page.')
+
+    expect((await page.goto(`${host}/baz`))?.status()).toBe(404)
+    expect(page.url()).toEqual(`${host}/baz`)
+    expect(await page.content()).toContain('Woops, page not found!')
+}
